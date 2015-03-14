9d8ac0c595c10a51d7b36f4935af420faa56ecc9
Julien Viet <julien@julienviet.com>
2015-03-14 14:02:15 +0100

    Reformat and remove \t

--- src/main/resources/archetype-resources/src/test/java/MainVerticleTest.java
+++ src/main/resources/archetype-resources/src/test/java/MainVerticleTest.java
@@ -18,46 +18,46 @@ import org.junit.runner.RunWith;
 @RunWith(VertxUnitRunner.class)
 public class MainVerticleTest {
 
-  private Vertx vertx;
+    private Vertx vertx;
 
-  @Before
-  public void setUp(TestContext context) {
-    Async async = context.async();
-    vertx = Vertx.vertx();
-    vertx.deployVerticle(MainVerticle.class.getName(), ar -> {
-      if (ar.succeeded()) {
-        async.complete();
-      } else {
-        context.fail("Could not deploy verticle");
-      }
-    });
-  }
-
-  @Test
-  public void testHello(TestContext context) {
-    Async async = context.async();
-    HttpClient client = vertx.createHttpClient();
-    HttpClientRequest req = client.get(8080, "localhost", "/");
-    req.exceptionHandler(err -> {
-      context.fail();
-    });
-    req.handler(resp -> {
-      context.assertEquals(200, resp.statusCode());
-      Buffer entity = Buffer.buffer();
-      resp.handler(entity::appendBuffer);
-      resp.endHandler(v -> {
-        context.assertEquals("Hello World", entity.toString("UTF-8"));
-        async.complete();
+    @Before
+    public void setUp(TestContext context) {
+      Async async = context.async();
+      vertx = Vertx.vertx();
+      vertx.deployVerticle(MainVerticle.class.getName(), ar -> {
+          if (ar.succeeded()) {
+              async.complete();
+          } else {
+              context.fail("Could not deploy verticle");
+          }
       });
-    });
-    req.end();
-  }
+    }
 
-  @After
-  public void tearDown(TestContext context) {
-    Async async = context.async();
-    vertx.close(ar -> {
-      async.complete();
-    });
-  }
+    @Test
+    public void testHello(TestContext context) {
+        Async async = context.async();
+        HttpClient client = vertx.createHttpClient();
+        HttpClientRequest req = client.get(8080, "localhost", "/");
+        req.exceptionHandler(err -> {
+            context.fail();
+        });
+        req.handler(resp -> {
+            context.assertEquals(200, resp.statusCode());
+            Buffer entity = Buffer.buffer();
+            resp.handler(entity::appendBuffer);
+            resp.endHandler(v -> {
+                context.assertEquals("Hello World", entity.toString("UTF-8"));
+                async.complete();
+            });
+        });
+        req.end();
+    }
+
+    @After
+    public void tearDown(TestContext context) {
+        Async async = context.async();
+        vertx.close(ar -> {
+            async.complete();
+        });
+    }
 }
