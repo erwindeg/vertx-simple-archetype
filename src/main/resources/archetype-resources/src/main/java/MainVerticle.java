1d3bc53a81d2c3a3ab638b20e28021f399192e42
Erwin De Gier <Erwin@surface.(none)>
2015-03-14 10:36:56 +0100

    inital import

---
+++ src/main/resources/archetype-resources/src/main/java/MainVerticle.java
@@ -0,0 +1,20 @@
+#set( $symbol_pound = '#' )
+#set( $symbol_dollar = '$' )
+#set( $symbol_escape = '\' )
+package ${package};
+
+import io.vertx.core.AbstractVerticle;
+import io.vertx.core.DeploymentOptions;
+import io.vertx.core.http.HttpMethod;
+import io.vertx.core.http.HttpServerOptions;
+
+
+public class MainVerticle extends AbstractVerticle {
+
+    private static final int PORT = 8080;   
+    
+    @Override
+    public void start() throws Exception {
+		vertx.createHttpServer(new HttpServerOptions().setPort(PORT)).requestHandler(req -> req.response().end("Hello World")).listen();
+    }    
+}
